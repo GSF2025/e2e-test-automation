1d24fc53533676b078a6320ef65a091b9f7038b7
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-03-19 19:11:32 +0200

    Created Tests for Hamba Website Our Services Menu Dropdown

--- tests/menu.spec.ts
+++ tests/menu.spec.ts
@@ -1,10 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('should navigate to about page', async ({ page }) => {
-  await page.goto('https://website-04db7b0f.npd.wyr.mybluehost.me/staging/1386/');
+test.describe('Nav Menu Options', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://website-04db7b0f.npd.wyr.mybluehost.me/staging/1386/');
+    await expect(page).toHaveTitle(/Hamba/);
+  });
 
-  await expect(page).toHaveTitle(/Hamba/);
-  
-  await page.getByRole('link', { name: 'About' }).click();
-  await expect(page).toHaveTitle(/About/);
+  test('should navigate to about page', async ({ page }) => {
+
+    await page.getByRole('link', { name: 'About' }).click();
+    await expect(page).toHaveTitle(/About/);
+  });
+
+  // Testing for successful submenu click on nav bar
+  test('Should open a submenu when "Our Services" is clicked in the nav bar.', async ({page}) => {
+
+    await page.getByRole('link', { name: 'Our Services' }).click();
+    await expect(page.getByRole('link', {name: 'Market Place',})).toBeVisible();
+    await expect(page.getByRole('link', {name: 'Master Classes',})).toBeVisible();
+  });
 });
